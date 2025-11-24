423b9013a4a772ebf909dea7b1ddb708204f8c5d
DeagleTWC <canarybird0618@gmail.com>
2025-11-24 18:11:10 -0500

    Remove SnyderLogic demo code from demo.py

--- demo.py
+++ demo.py
@@ -1,10 +1,1 @@
-import streamlit as st
-from snyderlogic.core import SnyderLogic
 
-st.title("SnyderLogic Demo")
-sl = SnyderLogic()
-statement = st.text_input("Enter belief to update:")
-if st.button("Update"):
-    result = sl.update(statement)
-    st.write(result)
-st.download_button("Export Knowledge", json.dumps(sl.beliefs, indent=2), "snyder.json")
